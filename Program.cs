d3b3715fe5fb02661d19b0a61fe82856ce7c443e
drxjmarcelo <mendoncajoaom6@gmail.com>
2026-03-01 01:41:51 -0300

    CRUD completed

--- Program.cs
+++ Program.cs
@@ -9,15 +9,16 @@ class Program
 
         int opcao = -1;
 
-        while (opcao != 6)
+        while (opcao != 7)
         {
             Console.WriteLine("\n=== MENU ===");
             Console.WriteLine("1 - Novo cadastro");
             Console.WriteLine("2 - Logar");
             Console.WriteLine("3 - Checar Usuários Cadastrados");
-            Console.WriteLine("4 - Deletar Usuário");
-            Console.WriteLine("5 - Redefinir Banco de Dados");
-            Console.WriteLine("6 - Sair");
+            Console.WriteLine("4 - Atualizar Usuário");
+            Console.WriteLine("5 - Deletar Usuário");
+            Console.WriteLine("6 - Redefinir Banco de Dados");
+            Console.WriteLine("7 - Sair");
 
             if (!int.TryParse(Console.ReadLine(), out opcao))
             {
@@ -38,16 +39,20 @@ class Program
                 case 3:
                     auth.List();
                     break;
-
+                
                 case 4:
+                    auth.UpdateUser();
+                    break;
+
+                case 5:
                     auth.DeleteUser();
                     break;
                 
-                case 5:
+                case 6:
                     auth.Wipe();
                     break;
                 
-                case 6:
+                case 7:
                     Console.WriteLine("Saindo...");
                     break;
 
--- obj/Debug/net10.0/Encryption.AssemblyInfo.cs
+++ obj/Debug/net10.0/Encryption.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Encryption")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fbc81078e1432915fd0910ec7982bbf917f1162")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ee00016741cbac6bae974d871ef7075d235c925")]
 [assembly: System.Reflection.AssemblyProductAttribute("Encryption")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Encryption")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
